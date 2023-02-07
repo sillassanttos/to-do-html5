3b904bb9ccadfdcb4bfba4f66a831e278bdf35d9
Silas Santos <sillassanttos@outlook.com>
2023-02-07 17:17:06 -0400

    Adicionado inclusao e exclusão de tarefas com confirmação

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,69 @@
-let data = [{
-  id: 1,
-  title: "Estudar HTML"
-}, {
-  id: 2,
-  title: "Estudar CSS"
-}, {
-  id: 3,
-  title: "Estudar JavaScript"
-}];
+let data = [];
 
-data.forEach(task => {
+function renderTodo() {
+
+  document.querySelector('.todo').innerHTML = '';
+
+  data.forEach(task => {
+    
+    let li = document.createElement('li');
+    li.innerHTML = `
+      <input type="checkbox" id="task-${task.id}">
+      <label for="task-${task.id}">
+        ${task.title}
+      </label>
+      <button type="button">x</button>
+    `;
   
-  let li = document.createElement('li');
-  li.innerHTML = `
-    <input type="checkbox" id="task-${task.id}">
-    <label for="task-${task.id}">
-    ${task.title}
-    </label>
-  `;
+    li.querySelector('input').addEventListener("change", e => {
+      if (e.target.checked) {
+        li.classList.add('complete');
+      } else {
+        li.classList.remove('complete');
+      }
+    });
 
-  document.querySelector('.todo').append(li);
+    li.querySelector('button').addEventListener('click', e => {
+      let button = e.target;
+      let li = button.parentNode;
+      let input = li.querySelector('input');
+      let id = input.id;
+      let idArray = id.split('-');
+      let todoId = idArray[1];
+      let title = li.querySelector('label').innerText;
 
-  console.log(task.title);
+      if (confirm(`Deseja realmente excluir a tarefa ${title} ?`)) {
+
+        data = data.filter(task => task.id !== parseInt(todoId));
+  
+        renderTodo();
+      }
+
+    })
+  
+    document.querySelector('.todo').append(li);
+    
+  });
+
+
+}
+
+document.querySelector('#new-task').addEventListener('keyup', e => {
+
+  if (e.key === 'Enter') {
+
+    data.push({
+      id: data.length + 1,
+      title: e.target.value
+    });
+
+    e.target.value = '';
+
+    renderTodo();
+
+  }
+
 });
+
+
+renderTodo();
